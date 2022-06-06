4cac3390a13b764e2233bf287a3cfcdd53b0cf1b
Egli Marc (IT-PTR-SL4-YPT - Extern) <egli@puzzle.ch>
2022-06-06 16:10:47 +0200

    initial commit

---
+++ src/main/java/com/planningtool/egli/EgliApplication.java
@@ -0,0 +1,13 @@
+package com.planningtool.egli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planningtool/egli/EgliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planningtool.egli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
